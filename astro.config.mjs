48a837bda04bacf7d921b53b2c36ad1a3d1d6780
Gnadi <johannes.gnadlinger1997@gmail.com>
2024-06-07 21:01:19 +0200

    zwischenstand storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-
+import { loadEnv } from 'vite';
+import storyblok from '@storyblok/astro';
 import icon from "astro-icon";
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,5 +22,15 @@ export default defineConfig({
     drafts: true
   },
   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap(), mdx(), icon()]
+  integrations: [tailwind(), sitemap(), mdx(), icon(), storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      blogPost: 'storyblok/BlogPost',
+      blogPostList: 'storyblok/BlogPostList',
+      page: 'storyblok/Page',
+    },
+    apiOptions: {
+      region: 'eu',
+    },
+  })]
 });
