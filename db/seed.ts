270550d51c43bc507d93ac8a0c341ae308f85f29
gnadi <johannes.gnadlinger1997@gmail.com>
2024-08-05 19:36:19 +0200

    Revert "added seed.ts"
    This reverts commit 92c3115f7b0c7fd1df3697635340f7dbb31eef5f.

--- db/seed.ts
+++
@@ -1,8 +0,0 @@
-import { db, Contact } from 'astro:db';
-
-export default async function() {
-    await db.insert(Contact).values([
-        { id: 1, mail: "hello@world.com" },
-        { id: 2, email: "hell@world.com" },
-    ]);
-}
